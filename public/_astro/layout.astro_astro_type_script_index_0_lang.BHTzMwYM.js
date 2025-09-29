7806339b4055fc651c8604eff2e5123efc2dc0e1
Cristian Vega <cristianvega6150@gmail.com>
2025-09-28 23:27:00 -0500

    feat(ui): UI with api information

---
+++ public/_astro/layout.astro_astro_type_script_index_0_lang.BHTzMwYM.js
@@ -0,0 +1,1 @@
+import{$ as a,a as e}from"./nanos.CFHps0zk.js";const t=async()=>{let s=await(await fetch(`${a.value}/docs-api`)).json();e.set(s.data)};document.addEventListener("DOMContentLoaded",async()=>{t()});
